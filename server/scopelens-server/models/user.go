9ab1a2b2c4b200cc430cdf327715cf0863a1f725
txfs19260817 <zeminjiang@mail.ru>
2020-07-15 21:25:24 +0800

    add like to user

--- server/scopelens-server/models/user.go
+++ server/scopelens-server/models/user.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // The User holds
 type User struct {
-	ID       primitive.ObjectID `bson:"_id"      json:"id"`
-	UserName string             `bson:"username" json:"username" binding:"required"`
-	Email    string             `bson:"email"    json:"email"    binding:"required,email"`
-	Password string             `bson:"password" json:"password" binding:"required"`
+	ID       primitive.ObjectID   `bson:"_id"      json:"id"`
+	UserName string               `bson:"username" json:"username" binding:"required"`
+	Email    string               `bson:"email"    json:"email"    binding:"required,email"`
+	Password string               `bson:"password" json:"password" binding:"required"`
+	Like     []string             `bson:"like"     json:"like"`
 }
 
 // GetUserByName returns the User struct pointer by the given name.
@@ -25,17 +25,4 @@ func (d *DBDriver) GetUserByUsername(username string) (*User, error) {
 		return nil, err
 	}
 	return user, nil
-}
-
-// GetEmailByUsername returns the Email string by the given name.
-func (d *DBDriver) GetEmailByUsername(username string) (string, error) {
-	var res bson.M
-	opt := options.FindOne().SetProjection(bson.M{"_id": 0, "email": 1})
-	err := d.DB.Collection("users").
-		FindOne(context.Background(), bson.D{{Key: "username", Value: username}}, opt).
-		Decode(&res)
-	if err != nil {
-		return "", err
-	}
-	return res["email"].(string), nil
 }
