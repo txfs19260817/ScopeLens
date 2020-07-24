ec24de8c4a3a3a855fdf0a4144f9b98a145d0291
txfs19260817 <zeminjiang@mail.ru>
2020-07-24 21:09:57 +0800

    add official usage site

--- web/scopelens-website/src/views/Usage.vue
+++ web/scopelens-website/src/views/Usage.vue
@@ -1,34 +1,54 @@
 <template>
     <v-container>
-        <v-container class="searchbar fill-height">
-            <v-row align="center" justify="center" no-gutters>
-                <v-col>
-                    <v-card class="elevation-2 card">
-                        <v-card-text>
-                            <h1 class="text-start display-1 mb-10 primary--text"> {{$t('usage.title')}} </h1>
-                            <v-form class="searchbar-form" @submit.prevent="getUsage">
-                                <FormatSelector :value.sync="format" :hint="$t('upload.hint.format')"></FormatSelector>
-                                <div class="text-center mt-6">
-                                    <v-btn :class="{'grey--text text--darken-4': $vuetify.theme.dark}" color="primary" type="submit" large dark :loading="loading">
-                                        <v-icon left dark>search</v-icon>
-                                        {{$t('usage.btn')}}
-                                    </v-btn>
-                                </div>
-                            </v-form>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-        <v-container v-show="usage.length > 0" class="searchbar fill-height">
-            <v-row align="center" justify="center" no-gutters>
-                <v-card class="elevation-2 card">
-                    <v-card-text>
-                        <div id="pie"></div>
-                    </v-card-text>
-                </v-card>
-            </v-row>
-        </v-container>
+        <v-tabs class="fill-height" grow slider-color="primary" background-color="bg_primary">
+            <v-tab class="font-weight-bold title">{{ $t('usage.tabs.official') }}</v-tab>
+            <v-tab-item class="text-center">
+                <div class="hidden-xs-only tab-item-wrapper">
+                    <iframe :src="officialSrc"></iframe>
+                </div>
+                <v-btn :class="{'grey--text text--darken-4': $vuetify.theme.dark, 'hidden-sm-and-up': true}"
+                       style="margin: 20px" :href="officialSrc" target="_blank"
+                       color="primary" type="submit" large dark :loading="loading">
+                    <v-icon left dark>mdi-open-in-new</v-icon>
+                    {{ $t('usage.newWindow') }}
+                </v-btn>
+            </v-tab-item>
+            <v-tab class="font-weight-bold title">{{ $t('usage.tabs.website') }}</v-tab>
+            <v-tab-item>
+                <v-container class="searchbar fill-height">
+                    <v-row align="center" justify="center" no-gutters>
+                        <v-col>
+                            <v-card class="elevation-2 card">
+                                <v-card-text>
+                                    <h1 class="text-start display-1 mb-10 primary--text"> {{$t('usage.title')}} </h1>
+                                    <v-form class="searchbar-form" @submit.prevent="getUsage">
+                                        <FormatSelector :value.sync="format"
+                                                        :hint="$t('upload.hint.format')"></FormatSelector>
+                                        <div class="text-center mt-6">
+                                            <v-btn :class="{'grey--text text--darken-4': $vuetify.theme.dark}"
+                                                   color="primary" type="submit" large dark :loading="loading">
+                                                <v-icon left dark>search</v-icon>
+                                                {{$t('usage.btn')}}
+                                            </v-btn>
+                                        </div>
+                                    </v-form>
+                                </v-card-text>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <v-container v-show="usage.length > 0" class="searchbar fill-height">
+                    <v-row align="center" justify="center" no-gutters>
+                        <v-card class="elevation-2 card">
+                            <v-card-text>
+                                <div id="pie"></div>
+                            </v-card-text>
+                        </v-card>
+                    </v-row>
+                </v-container>
+            </v-tab-item>
+        </v-tabs>
+
     </v-container>
 </template>
 
@@ -51,6 +71,7 @@
                 usage: [],
                 // Sprites paths
                 iconUrl: process.env.VUE_APP_STATIC_ASSET_URL + '2d/',
+                officialSrc: "https://resource.pokemon-home.com/battledata/rankmatch_detail.html?l=9&c=302&v=443199&lcc=1595518253"
             }
         },
         methods: {
@@ -124,6 +145,12 @@
             loading() {
                 return this.$store.state.loading.loading
             },
+            tabs() {
+                return [
+                    this.$i18n.t('usage.tabs.official'),
+                    this.$i18n.t('usage.tabs.website'),
+                ]
+            }
         },
         created() {
             this.echarts = echarts;
@@ -156,11 +183,28 @@
             height: 400px;
         }
     }
+
     @media screen and (max-width: 420px) {
         #pie {
             width: 280px;
             height: 280px;
         }
+    }
+
+    .tab-item-wrapper {
+        /* vuetify sets the v-tabs__container height to 48px */
+        height: calc(100vh - 160px);
+        overflow: auto;
+        -webkit-overflow-scrolling:touch;
+        width:100%;
+    }
+
+    iframe {
+        border:none;
+        width: 1px;
+        min-width: 100%;
+        *width: 100%;
+        height: 100%;
     }
 
 </style>
