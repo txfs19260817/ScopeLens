cec9989c8d4fa1e3ba334d2f8d65887e82b2661a
txfs19260817 <duola865@gmail.com>
2021-06-07 17:59:15 +0800

    update k8s settings

--- website/src/api/predict.js
+++ website/src/api/predict.js
@@ -1,14 +1,13 @@
 import http from "./index";
 
+const advapiUrl = process.env.VUE_APP_ADVANCED_URL + "predict";
+
 export const getSpeciesByImage = (file) => {
     const headers = {
         'Content-Type': 'multipart/form-data',
     }
     let formData = new FormData();
     formData.append('file', file);
-    return http.post(process.env.VUE_APP_ADVANCED_URL+"predict", formData, {
-        params:{
-            type: 'team'
-        }
-    })
+    // a baseURL overriding
+    return http({ method: 'post', url: advapiUrl, baseURL: '/', data: formData, params: { type: 'team' } })
 }
